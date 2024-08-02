001aeb2f2ed74c36e21a3d2a1f2ac82bf0a5bef8
wastedroses <99488803+wastedroses@users.noreply.github.com>
2024-08-02 09:55:07 -0400

    Add files via upload

---
+++ HelloJack.java
@@ -0,0 +1,36 @@
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+
+/**
+ * Put a short phrase describing the program here.
+ *
+ * @author Put your name here
+ *
+ */
+public final class HelloJack {
+
+    /**
+     * No argument constructor--private to prevent instantiation.
+     */
+    private HelloJack() {
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        SimpleReader in = new SimpleReader1L();
+        SimpleWriter out = new SimpleWriter1L();
+        out.println("What's your name?");
+        String name = in.nextLine();
+        out.println("Hello, " + name);
+        in.close();
+        out.close();
+    }
+
+}
